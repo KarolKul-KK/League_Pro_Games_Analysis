c5c866a86cf4d7d4a16ad4710a4215c7c1467176
KarolKul-KK <>
2022-03-13 20:20:19 +0100

    Added url scrapper and dir to containing data

---
+++ urls_scrap.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+from bs4 import BeautifulSoup
+import pandas as pd
+
+import time
+import re
+
+
+url = "https://gol.gg/esports/home/"
+driver = webdriver.Safari()
+driver.get(url)
+
+def get_urls(urls: list) -> None:
+        
+    data = []
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html5lib')
+    time.sleep(1)
+    table = soup.find('table')
+    
+    
+    for td in table.findAll('td', attrs = {'class': 'footable-visible'}):
+        for a in td.findAll('a'):
+            data.append(a['href'])
+    data = data[1:]
+    
+    for i in range(0, len(data), 12):
+        urls.append(data[i])
+
+
+def main() -> None:
+
+    urls = []
+    while True:
+        get_urls(urls)
+        time.sleep(3)
+        driver.find_element_by_xpath('//*[@id="previous10"]').click()
+
+    else:
+        pd.Series(urls).to_csv('data/games_urls.csv')
